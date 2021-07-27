bf3481c0b9ec2b49559a3b675960f9920c84247f
euberdeveloper <euberdeveloper@gmail.com>
2021-07-27 23:23:44 +0200

    style: configure eslint
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/.eslintrc.js
+++ source/.eslintrc.js
@@ -2,17 +2,12 @@ const path = require('path');
 
 module.exports = {
     parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
         project: path.join(__dirname, 'tsconfig.json')
     },
-    plugins: ['prettier'],
+    plugins: ['@euberdeveloper'],
     extends: [
-        '@euberdeveloper/typescript',
-        'plugin:prettier/recommended'
+        'plugin:@euberdeveloper/typescript',
+        'plugin:@euberdeveloper/unicorn',
+        'plugin:@euberdeveloper/prettier'
     ],
-    rules: {
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-misused-promises': 'off'
-    }
 };
